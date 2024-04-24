25db2b89eb0c7aa2ab4ff00389079adac56a7bae
gabsoares <soaresgabriel68@gmail.com>
2024-04-24 00:34:09 -0300

    Adicionar arquivos de projeto.

---
+++ OrdenarArray/Program.cs
@@ -0,0 +1,42 @@
+﻿int[] arrayOriginal = new int[6];
+int[] arrayOrdenado;
+int aux;
+Random random = new Random();
+
+for (int i = 0; i < 6; i++)
+{
+    arrayOriginal[i] = random.Next(1, 60);
+
+}
+arrayOrdenado = arrayOriginal;
+
+for (int i = 0; i < 6; i++)
+{
+    for (int j = (i + 1); j < 6; j++)
+    {
+        if (arrayOrdenado[i] != arrayOrdenado[j])
+        {
+            if (arrayOrdenado[i] > arrayOrdenado[j])
+            {
+                aux = arrayOrdenado[j];
+                arrayOrdenado[j] = arrayOrdenado[i];
+                arrayOrdenado[i] = aux;
+            }
+        }
+        else
+        {
+            arrayOrdenado[j] = 0;
+            if (arrayOrdenado[j] == 0)
+            {
+
+            }
+        }
+    }
+}
+
+
+for (int i = 0; i < 6; i++)
+{
+    Console.Write(arrayOrdenado[i] + " ");
+}
+Console.WriteLine();
